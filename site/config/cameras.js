07199f98f7cc2ee891029df41f85b7c09ecc0ada
FelipeWelter <feliwelter45@gmail.com>
2026-08-30 12:47:17 +0000

    envio att

--- site/config/cameras.js
+++ site/config/cameras.js
@@ -8,9 +8,9 @@
  */
 
 window.CAMERAS = [
-  { id: 'cam01', name: 'Câmera 01', location: 'BLOCO E', hls: '/hls/cam01/index.m3u8' },
-  { id: 'cam02', name: 'Câmera 02', location: 'TEATRO NACIONAL', hls: '/hls/cam02/index.m3u8' },
-  { id: 'cam03', name: 'Câmera 03', location: 'BLOCO R', hls: '/hls/cam03/index.m3u8' },
-  { id: 'cam04', name: 'Câmera 04', location: 'BLOCO J', hls: '/hls/cam04/index.m3u8' },
-  { id: 'cam05', name: 'Câmera 05', location: 'VTR C2', hls: '/hls/cam05/index.m3u8' }
+  { id: 'cam01', name: 'Bloco E ( MCTI )', location: 'Câmera 1', hls: '/hls/cam01/index.m3u8' },
+  { id: 'cam02', name: 'Teatro Nacional', location: 'Câmera 2', hls: '/hls/cam02/index.m3u8' },
+  { id: 'cam03', name: 'Bloco R ( MCOM )', location: 'Câmera 3', hls: '/hls/cam03/index.m3u8' },
+  { id: 'cam04', name: 'Viatura C2', location: 'Câmera 4', hls: '/hls/cam04/index.m3u8' },
+  { id: 'cam05', name: 'Bloco J ( MDIC )', location: 'Câmera 5', hls: '/hls/cam05/index.m3u8' }
 ];
